5425582ca6b5e312b1185a9e35f163c4b1624fe0
mihaelasabin <mihaela.sabin@unh.edu>
2020-03-31 20:09:16 -0400

    created starter code

---
+++ problems/data_analysis.py
@@ -0,0 +1,46 @@
+"""
+data_analysis.py
+(replace this with short statement about the purpose of the module)
+(replace this with your name)
+(replace this with two lines: created date and updated date)
+"""
+
+
+class DevStats():
+    """
+    (replace this with short statement about the purpose of the class)
+    """
+    @classmethod
+    def count_developers_by_country(cls, filename):
+        """
+        (replace with complete docstring.
+        It is an adaptation of  Activity 2 in StackOverflow Developers Survey)
+        filename: string that has the name of the CSV text file
+        Returns: dictionary in which keys are the country names and values are
+            the number of devevelopeoprs in each counntry
+        """
+
+    @classmethod
+    def average_salary_by_gender(cls, filename):
+        """
+        (replace with complete docstring.
+        It is an adaptation of  Activity 5 in StackOverflow Developers Survey
+        (add description of paramter)
+        (add description of returns)
+        """
+
+
+def main():
+    """
+    (replace with short statement about the purpose of this function)
+    """
+
+    # simplest test case for count_develooperers_by_country is simplecount1.csv
+    # that has one country, xxx, with yyy developeoprers
+    filename = 'simplecount1.csv'
+    actual_result = DevStats.count_developers_by_country(filename)
+    print(f'developers by country in {filename} are: {actual_result}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_count_developers_by_country.py
@@ -0,0 +1,36 @@
+"""
+(replace with module docstring; see data_analysis.py instructions)
+"""
+
+import unittest
+from problems.data_analysis import DevStats
+
+
+class TestCountDevelopersByCountry(unittest.TestCase):
+    """
+    (add your docstring; see lab5 example)
+    """
+    def setUp(self):
+        """
+        (add your docstring; see lab5 example)
+        (not sure whether we need this method)
+        """
+
+    def test_brief_count_developers_by_country(self):
+        """
+        (add your docstring)
+        """
+        # Remove these two statements below. Were added to remove pylint errors.
+        DevStats.count_developers_by_country('fake.csv')
+        self.assertDictEqual({}, {})
+        
+        # define and initialize test_case
+        # define and initialize actual_result by calling method with test_case
+        # argument
+        # define and initalize expected_result wiht data obtained through
+        # other means than calling the method
+        # call self.assertDictEqual(actual_result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
